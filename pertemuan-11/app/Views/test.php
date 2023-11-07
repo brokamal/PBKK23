37ad7e4194f5c0d280a98be108492eb7d3a74670
Kamal <125541437+ccunit@users.noreply.github.com>
2023-11-07 07:58:18 +0700

    Add files via upload

---
+++ pertemuan-11/app/Views/test.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Testing</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            text-align: center;
+        }
+        .container {
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            padding: 20px;
+            margin: 20% auto;
+            max-width: 400px;
+        }
+        h1 {
+            color: #333;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1><?php echo "Testing View"; ?></h1>
+    </div>
+</body>
+</html>
